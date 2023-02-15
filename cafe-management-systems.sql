dd7c1cba06a834d8b7d11382a506b23dff9da2a7
NVRSHarsnini <95141207+NVRSHarsnini@users.noreply.github.com>
2023-02-15 06:20:34 +0530

    Update cafe-management-systems.sql

--- cafe-management-systems.sql
+++ cafe-management-systems.sql
@@ -1,4 +1,4 @@
---drop database cafe_mgmt
+drop database cafe_mgmt
 create database cafe_mgmt
 use cafe_mgmt
 --Customer
@@ -11,14 +11,15 @@ payment_id nvarchar(30))
 
 --Menu
 create table Menu(
-menuid int primary key,
-itemid int,
+item_id int primary key,
 item_name varchar(30),
-price int ,
+iprice decimal(10,2),
+quantity int,
+category varchar(10),
 custid int,
-foreign key(custid) references Customer(cid),
-foreign key(itemid) references items(itemid));
+foreign key(custid) references Customer(cid));
 
+/*
 --items
 create table items(
 itemid int primary key,
@@ -26,7 +27,10 @@ iname varchar(30),
 iprice decimal(10,2),
 quantity int,
 category varchar(10))
+*/
 
+
+/*
 --orderitem
 create table orderitem
 (
@@ -35,16 +39,17 @@ itemid int,
 quantity int,
 unitprice decimal(10,2),
 foreign key(itemid) references items(itemid)
-)
+)*/
 
 --orders
 create table orders(
 oid int primary key,
-order_id int,
+item_id int,
+unitprice decimal(10,2),
 custid int,
 quantity int,
-foreign key(order_id)references orderitem(order_id),
-foreign key(custid) references customer(cid))
+foreign key(item_id)references Menu(item_id),
+foreign key(custid) references customer(cid));
 
 --Bill
 create table Bill(
@@ -59,6 +64,7 @@ fid int primary key,
 custid int,
 irating int,
 foreign key(custid) references customer(cid))
+
 
  
 
